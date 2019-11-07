d6894304fe2827cd8976dedaff1ebf96deab1df0
kampfknoedel <lukas.kuder@web.de>
2019-11-07 14:21:12 +0100

    set caption geändert

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -17,9 +17,9 @@ class GameWindow : public Gosu::Window
 public:
 	Gosu::Image bild;
 	GameWindow()
-		: Window(800, 600)
+		: Window(1920, 1080)
 	{
-		set_caption("Gosu Tutorial Game mit Git");
+		set_caption("PAPER.IO");
 	}
 
 	// wird bis zu 60x pro Sekunde aufgerufen.
